672a96e78d8da09bca050a952f08378427fe1a29
Eugene-Fed <eugene.a.fed@gmail.com>
2023-12-16 23:37:29 +0300

    review_01_step_03

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # devman_chat-bot-01
 
 ## Start and Config
-- В папке `config/` необходимо скопировать файл `example.env` и вставить в ту же папку с именем `.env`.
+- В корне необходимо скопировать файл `example.env` и вставить в ту же папку с именем `.env`.
 - Указать корректные токены в соответствующие переменные.  
-`DEVMAN_TOKEN` - токен пользователя API в Девман
-`TG_BOT_TOKEN`- токен ТГ бота
-`TG_CHAT_ID` - ID чата в личных сообщениях, в который бот будет отправлять оповещения
+`DEVMAN_TOKEN` - токен пользователя API в Девман  
+`TG_BOT_TOKEN`- токен ТГ бота  
+`TG_CHAT_ID` - ID чата в личных сообщениях, в который бот будет отправлять оповещения  
 
 ## Features
 - Получить имя пользователя:
@@ -27,6 +27,5 @@ firts_name = updates[-1].message.from_user.first_name
   "message": "Mentor has checked your work!\nTheme: {title}\nLink: {url}\n\n{review}",
   "review_messages": ["Unfortunately there are errors in the work :(",
     "Everything is fine! You can proceed to the next lesson :)"],
-  "error_messages": {"500":  "Server returned code 500. New request will be send in {} seconds."}
 }
 ```
--- main.py
+++ main.py
@@ -9,18 +9,13 @@ from dotenv import load_dotenv
 
 
 def load_config():
-    """
-    В корне проекта достаточно много "обязательного мусора" при наличии докера и разного CI/CD. Поэтому удобнее
-    `.env` хранить в какой-нибудь папке. По крайней мере на моем последнем проекте он хранился именно в `config/` -
-    это было единообразно по всем микросервисам и было вполне удобно.
-    """
-    dotenv_path = os.path.join(os.path.dirname(__file__), 'config', '.env')
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
     config_path = os.path.join(os.path.dirname(__file__), 'config', 'config.json')
+
     if os.path.exists(dotenv_path):
         load_dotenv(dotenv_path)
     if not os.environ.get('DEVMAN_TOKEN') or not os.environ.get('TG_BOT_TOKEN') or not os.environ.get('TG_CHAT_ID'):
-        raise EnvironmentError("You should set your `config/.env` file. Please check README.md.")
-
+        raise EnvironmentError("You should set your `.env` file. Please check README.md.")
     if os.path.exists(config_path):
         return json.load(open(config_path, "r"))
     else:
@@ -29,7 +24,7 @@ def load_config():
 
 def main():
     logging.basicConfig(
-        filename='app.log',  # Чтобы не раздувать структуру папок оставим в учебных целях логфайл в корне.
+        filename=os.path.join(os.path.dirname(__file__), 'log', 'app.log'),
         filemode='a',
         format='%(asctime)s: %(message)s',
         datefmt='%d/%m/%Y %H:%M:%S',
@@ -50,6 +45,8 @@ def main():
             response = requests.get(
                 reviews_url, headers=reviews_headers, timeout=config["timeout"], params=reviews_params
             )
+            response.raise_for_status()
+            review_info = response.json()
 
         except requests.exceptions.ReadTimeout:
             continue
@@ -57,29 +54,19 @@ def main():
             logging.warning(f"Connection Error. Waiting connection for {config['sleep']} seconds.")
             time.sleep(config["sleep"])
             continue
-
-        if response.status_code >= 500:
-            logging.warning(f"`{response.text}`. Request will be resend in {config['timeout']} seconds.")
-            time.sleep(config['timeout'])
-            continue
-        elif response.status_code == 404:  # Добавил перехват конкретных кодов согласно ТЗ. Остальные группируются.
-            raise requests.exceptions.HTTPError(f"{response.status_code}. Check `user_reviews` in `config.json`.")
-        elif response.status_code == 401:
-            raise requests.exceptions.HTTPError(f"{response.status_code}. Please check your DEVMAN_TOKEN.")
-        elif not response.ok:
-            raise requests.exceptions.HTTPError(f"{response.status_code}: `{response.text}`.")
-
-        try:
-            resp = response.json()  # Чтобы не впихивать в try/except весь следующий блок кода, проверяем json заранее
+        except requests.exceptions.HTTPError as ex:
+            logging.error(f"Server returned: `{ex.response}`.")
+            break
         except requests.exceptions.JSONDecodeError:
-            print("An unexpected result was returned. Check if DEVMAN_TOKEN and url for user_reviews are correct.")
+            logging.error(f"There is no JSON in the server response. "
+                          f"Check if DEVMAN_TOKEN and url for user_reviews are correct.")
             break
 
-        if ts := resp.get("timestamp_to_request"):  # Проверка на Timeout сервера
+        if ts := review_info.get("timestamp_to_request"):  # Проверка на Timeout сервера
             reviews_params["timestamp"] = ts
             continue
 
-        if attempts := resp.get("new_attempts"):
+        if attempts := review_info.get("new_attempts"):
             for attempt in attempts:
                 should_be_rewrite = attempt.get("is_negative")
                 review_message = config.get("review_messages")[0] if should_be_rewrite \
@@ -90,7 +77,7 @@ def main():
                                                       review=review_message),
                     chat_id=tg_chat_id)
                 logging.info("Message sent.")
-        if ts := resp.get("last_attempt_timestamp"):  # Поиск таймстампа в ответе с инфой.
+        if ts := review_info.get("last_attempt_timestamp"):  # Поиск таймстампа в ответе с инфой.
             reviews_params["timestamp"] = ts
 
 
